727fec199671db9c953e8b51cddc4c306436fb61
Angel Vanegas <angelman@gmail.com>
2018-03-16 11:32:32 -0400

    coin converting code

---
+++ coin_convert.rb
@@ -0,0 +1,80 @@
+module CoinConvert
+    QUARTER_VALUE = 25
+    DIME_VALUE = 10
+    NICKEL_VALUE = 5
+    PENNY_VALUE = 1
+
+    def self.make_money(amount)
+        amount
+        .chars
+        .reject{|x| x.eql?('.')}
+        .join
+        .to_i
+    end
+    
+    def self.change_returned(amount)
+        amount < 0 ? ["not enough"] : CoinConvert.denominations(amount)
+    end
+    
+    def self.convert(result)
+        # result = make_money(amount)
+        penny(result)
+    end
+
+    def self.quarters(amount)
+        {res: amount / QUARTER_VALUE, rem: amount % QUARTER_VALUE}
+    end
+    
+    def self.dimes(amount)
+        q = quarters(amount)
+        d = q[:rem]
+        {q: q[:res], d: d / DIME_VALUE, rem: d % DIME_VALUE}
+    end
+
+    def self.nickels(amount)
+        coins = dimes(amount)
+        n = coins[:rem]
+        {q: coins[:q], d: coins[:d], n: n / NICKEL_VALUE, rem: n % NICKEL_VALUE }
+    end
+
+    def self.penny(amount)
+        coins = nickels(amount)
+        pe = coins[:rem]
+        {q: coins[:q], d: coins[:d], n: coins[:n], p: pe}
+    end   
+    
+    class Coin
+        def initialize(amount)
+            @amount = amount
+        end  
+
+        def count
+            str = "#{@amount} #{self.class.name.split('::').last}"
+            @amount < 2 ? str : str + 's'
+        end
+    end
+    class Penny < Coin
+        def count
+            str = @amount.to_s
+            @amount < 2 ? str + ' Penny' : str + " Pennies"
+        end
+    end
+
+    class Quarter < Coin; end
+
+    class Dime < Coin; end
+
+    class Nickel < Coin; end
+
+    def self.denominations price
+        coins = convert(price)
+        [ 
+            Quarter.new(coins[:q]).count,
+            Dime.new(coins[:d]).count,
+            Nickel.new(coins[:n]).count,
+            Penny.new(coins[:p]).count
+        ]
+    end
+
+    private_class_method :quarters, :dimes, :nickels, :penny, :convert
+end
---
+++ stuff.rb
@@ -0,0 +1,25 @@
+require_relative 'coin_convert'
+
+# Description: Create a program where it shows the user a list of items to buy and their price. 
+# Then ask the user to pick an item and enter in the amount of money they would have inserted into the “vending 
+# machine”. Have the program calculate the change and return it to the user in the form of quarters, dimes, nickels 
+# and pennies. For example, if the user chooses an item that costs $1.25 and they say they give it $2.07 the program 
+# would print out 3 quarters, 1 nickel and 2 pennies as change for the user. 
+
+items = {sandwich: "3.36", soda: "0.75"}
+
+puts items
+puts "What item?"
+food = gets.chomp
+
+puts "How much?"
+price = gets.chomp
+
+amount  = CoinConvert.make_money(price) - CoinConvert.make_money(items[food.to_sym])
+
+coins = CoinConvert.change_returned(amount)
+
+puts "Here's your change:"
+coins.reject {|x| x.split(' ').first.to_i == 0 }.each do |coin|
+    puts coin
+end
